403f36513941ce91c3c32f68cf049a441c18200c
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2022-12-06 12:46:49 -0500

    Get value of main club sheet
    get main club sheet value, so i can copy people in attendence

--- Controllers/createAttendenceDateController.js
+++ Controllers/createAttendenceDateController.js
@@ -3,6 +3,13 @@ exports.createAttendeceDate = async (req, res) => {
   try {
     const sheets = req.object.sheets; //this is needed to get google spreadsheet data
 
+    const getRows = await sheets.spreadsheets.values.get({
+      spreadsheetId: req.sheetID,
+      range: "Information",
+    });
+    const sheetValues = getRows.data.values;
+    console.log(sheetValues);
+
     //this is what adds spread inside club spreadsheet
     await sheets.spreadsheets.batchUpdate({
       spreadsheetId: req.sheetID,
