e2870e7b4fa2ebdb677af0628fe9f93ce02845fe
HaoChenSupreme <HAoranc3@nycstudents.net>
2023-01-05 11:46:43 -0500

    fefefsd

--- Controllers/clubcode.js
+++ Controllers/clubcode.js
@@ -7,26 +7,26 @@ const MainClubData = "1Xm649d7suBlRVjXJeH31k4mAq3NLFV8pW_8QrJ55QpU";
 const UserData = "1noJsX0K3kuI4D7b2y6CnNkUyv4c5ZH-IDnfn2hFu_ws";
 const ClubCode = "FakeCode";
 const UserID = "114999107574796439138";
-const { IDs } = await verifyToken(incomingUserData.accessToken);
-const incomingUserData = req.body.userCredential;
+// const { IDs } = await verifyToken(incomingUserData.accessToken);
+// const incomingUserData = req.body.userCredential;
 
 
 
-const auth = new google.auth.GoogleAuth({
-    keyFile: "keys.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
+// const auth = new google.auth.GoogleAuth({
+//     keyFile: "keys.json",
+//     scopes: "https://www.googleapis.com/auth/spreadsheets",
+//   });
 
 
-async function verifyToken(token) {
-    client.setCredentials({ access_token: token });
-    const userinfo = await client.request({
-      url: "https://www.googleapis.com/oauth2/v3/userinfo",
-    });
-    return userinfo.data;
-}
+// async function verifyToken(token) {
+//     client.setCredentials({ access_token: token });
+//     const userinfo = await client.request({
+//       url: "https://www.googleapis.com/oauth2/v3/userinfo",
+//     });
+//     return userinfo.data;
+// }
 
-
+// this compares the user's id to the various ids on "UserData" 
 if (data.sub === UserID) {
     const { IDs } = await googleSheets.spreadsheets.values.get({
         auth,
