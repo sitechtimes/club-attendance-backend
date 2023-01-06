e2ac5e1d24e6a1aa6b8168d46afe35f7cd52534b
HaoChenSupreme <HAoranc3@nycstudents.net>
2023-01-06 12:32:12 -0500

    efddv

--- Controllers/clubcode.js
+++ Controllers/clubcode.js
@@ -12,10 +12,10 @@ const UserID = "114999107574796439138";
 
 
 
-// const auth = new google.auth.GoogleAuth({
-//     keyFile: "keys.json",
-//     scopes: "https://www.googleapis.com/auth/spreadsheets",
-//   });
+const auth = new google.auth.GoogleAuth({
+    keyFile: "keys.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
 
 
 // async function verifyToken(token) {
@@ -26,97 +26,8 @@ const UserID = "114999107574796439138";
 //     return userinfo.data;
 // }
 
-// this compares the user's id to the various ids on "UserData" 
-if (data.sub === UserID) {
-    const { IDs } = await googleSheets.spreadsheets.values.get({
-        auth,
-        UserData,
-        range: "A1:A",
-    });
-    const values = IDs.values;
-    let rowNum;
-    for(let i = 0; i < values.length; i++) {
-        if (values[i] === UserID) {
-            rowNum = i + 1;
-            break;
-        }
-    }
-    const { IDs } = await googleSheets.spreadsheets.values.update({
-        auth,
-        UserData,
-        range: `H${rowNum}:H${rowNum}`,
-        valueInputOption: "USER_ENTERED",
-        values: [[ClubCode]],
-    });
-}
-
-const Codes = await googleSheets.spreadsheets.values.get({
+const IDs = await googleSheets.spreadsheets.values.get({
     auth,
     MainID,
     range: 'Information!L2:L',
 });
-
-const users = await googleSheets.spreadsheets.values.get({
-    auth,
-    UserData,
-    range: 'UserData!L2:L',
-});
-
-let ClubSheet;
-
-for (let i = 0; i < Codes.data.values.length; i++) {
-    const clubcell = Codes.data.values[i];
-    if (clubcell[0] === ClubCode) {
-        ClubSheet = clubcell[1];
-    }
-}
-
-for (let i = 0; i < users.data.values.length; i++) {
-    const usercell = users.data.values[i];
-    if (usercell[0] === UserID) {
-        usercell[1] = ClubCode;
-    }
-}
-
-await googleSheets.spreadsheets.values.update({
-    auth,
-    UserData,
-    range: 'UserData!H2',
-    valueInputOption: "USER_ENTERED",
-    resource: {
-        values: [
-            [UserID, ClubCode],
-        ]
-    },
-});
-
-return ClubSheet;
-
-
-async function modifySpreadsheet(auth) {
-    const sheets = google.sheets({ version: "v4", auth });
-    const sheetId = await sheets.spreadsheets.values.get({
-    spreadsheetId: MainClubData,
-    range: "L:L",
-    }).then(({ data }) => {
-    const row = data.values.findIndex(
-    (value) => value[0] === ClubCode
-    );
-    if (row === -1) {
-    return;
-    }
-    const sheetId = data.values[row][1];
-    sheets.spreadsheets.values.append({
-    spreadsheetId: sheetId,
-    range: "A:A",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-    values: [
-    [UserID, incomingUserData],
-    ],
-    },
-    }).then(() => console.log("Data appended into spreadsheet"));
-    });
-    }
-    
-    modifySpreadsheet(auth);
