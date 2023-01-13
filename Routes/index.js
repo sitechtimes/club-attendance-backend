3b175afd54987e4ff2cad7cc028964442a6402a3
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2023-01-13 12:40:44 -0500

    Update index.js

--- Routes/index.js
+++ Routes/index.js
@@ -11,12 +11,7 @@ const getAttendence = require("../Controllers/getAttendenceController");
 const login = require("../Controllers/verificationMiddleware");
 const addClub = require("../Controllers/clubcode");
 
-
-router.get(
-  "/addclub",
-  addClub.addClubCode,
-  addClub.addUserDataToClub,
-);
+router.get("/addclub", addClub.addClubCode, addClub.addUserDataToClub);
 
 router.post(
   "/login",
