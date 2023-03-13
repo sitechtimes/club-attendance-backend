2cc738f8443ac21f38b9b47575a3e92ee0b48e9c
HaoChenSupreme <90791814+HaoChenSupreme@users.noreply.github.com>
2023-03-13 11:06:20 -0400

    fdsvr

---
+++ controllers/club/uploadPhotoController.js
@@ -0,0 +1,30 @@
+exports.uploadPhoto = async (req, res, next) => {
+    try {
+      const fs = require('fs');
+      const {GoogleAuth} = require('google-auth-library');
+      const {google} = require('googleapis');
+  
+      // Get credentials and build service
+      // TODO (developer) - Use appropriate auth mechanism for your app
+      const auth = new GoogleAuth({
+        scopes: 'https://www.googleapis.com/auth/drive',
+      });
+      const service = google.drive({version: 'v3', auth});
+      const requestBody = {
+        name: 'photo.jpg',
+        fields: 'id',
+      };
+      const media = {
+        mimeType: 'image/jpeg',
+        body: fs.createReadStream('files/photo.jpg'),
+      };
+      const file = await service.files.create({
+        requestBody,
+        media: media,
+      });
+      console.log('File Id:', file.data.id);
+      return file.data.id;
+    } catch (error) {
+      console.log(error);
+    }
+  };
