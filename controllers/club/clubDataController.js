6bf0115aaa531f6649eda7e078a245274ee882a8
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2023-05-23 18:24:27 -0400

    Change the sorting to be clubCode

--- controllers/club/clubDataController.js
+++ controllers/club/clubDataController.js
@@ -67,10 +67,11 @@ exports.ifClubExist = async (req, res, next) => {
     const ifClubExist = await ifValueExistBinary(
       sheets,
       CLUB_DATA_SPREADSHEET_ID,
-      `${clubRange}!A:A`,
-      userClub.clubName
+      `${clubRange}!P:P`,
+      userClub.clubCode
     );
-    console.log(ifClubExist, userClub.clubName);
+
+    console.log(ifClubExist, userClub.clubCode);
 
     if (ifClubExist === false) {
       console.log("no such club");
@@ -94,8 +95,8 @@ exports.returnSheetId = async (req, res, next) => {
       sheets,
       CLUB_DATA_SPREADSHEET_ID,
       clubRange,
-      userClub.clubName,
-      0
+      userClub.clubCode,
+      15
     );
 
     req.clubData = clubData;
--- utility.js
+++ utility.js
@@ -252,7 +252,6 @@ const getOneData = async (
   const newData = data.sort((a, b) => {
     return a[column] - b[column];
   });
- 
 
   let start = 0;
   let end = newData.length - 1;
