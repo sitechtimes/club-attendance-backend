39ed64fd889fbc29da53e345e00a656457113ee0
HaoChenSupreme <HAoranc3@nycstudents.net>
2023-05-26 12:28:47 -0400

    need to sepertate

--- controllers/club/clubcode.js
+++ controllers/club/clubcode.js
@@ -65,54 +65,69 @@ exports.addUserDataToClub = async (req, res) => {
     console.log(UserID, "userID");
     console.log(clubIDs.includes(UserID), "Does User Exist Already");
 
-    // This gets the user's data from the user data sheet
-    const clubData = await google
-      .sheets({ version: "v4", auth })
-      .spreadsheets.values.get({
-        spreadsheetId: userDataSheetID,
-        range: `A${userRowNumber}:H${userRowNumber}`,
+    if (clubIDs.includes(UserID) === true) {
+      res.json("Club Already Added");
+    } else {
+      // This gets the user's data from the user data sheet
+      const clubData = await google
+        .sheets({ version: "v4", auth })
+        .spreadsheets.values.get({
+          spreadsheetId: userDataSheetID,
+          range: `A${userRowNumber}:H${userRowNumber}`,
+        });
+      let UID = clubData.data.values[0][0];
+      let firstName = clubData.data.values[0][1];
+      let lastName = clubData.data.values[0][2];
+      let email = clubData.data.values[0][3];
+      let OSIS = clubData.data.values[0][5];
+      let grade = clubData.data.values[0][6];
+      let offClass = clubData.data.values[0][7];
+      console.log(clubData);
+      console.log(UID);
+      console.log(firstName);
+      console.log(lastName);
+      console.log(email);
+      console.log(OSIS);
+      console.log(grade);
+      console.log(offClass);
+
+      // Add user's data into the clubSheet (the club the user added)
+      google.sheets({ version: "v4", auth }).spreadsheets.values.append({
+        spreadsheetId: clubSheet,
+        range: "Sheet1!A:H",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: [
+            [UID, firstName, lastName, OSIS, `member`, grade, email, offClass],
+          ],
+        },
       });
-    let UID = clubData.data.values[0][0];
-    let firstName = clubData.data.values[0][1];
-    let lastName = clubData.data.values[0][2];
-    let email = clubData.data.values[0][3];
-    let OSIS = clubData.data.values[0][5];
-    let grade = clubData.data.values[0][6];
-    let offClass = clubData.data.values[0][7];
-    console.log(clubData);
-    console.log(UID);
-    console.log(firstName);
-    console.log(lastName);
-    console.log(email);
-    console.log(OSIS);
-    console.log(grade);
-    console.log(offClass);
 
-    // Add user's data into the clubSheet (the club the user added)
-    google.sheets({ version: "v4", auth }).spreadsheets.values.append({
-      spreadsheetId: clubSheet,
-      range: "Sheet1!A:H",
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: [
-          [UID, firstName, lastName, OSIS, `member`, grade, email, offClass],
-        ],
-      },
-    });
+      const specificClubIDs = await sheetData(sheets, clubSheet, "A:B");
+      console.log(specificClubIDs, "specificClubIDs");
+      const specificClubRowNumber = specificClubIDs.length + 1;
+      console.log(specificClubIDs.length, "clublength");
 
-    const specificClubIDs = await sheetData(sheets, clubSheet, "A:B");
-    console.log(specificClubIDs, "specificClubIDs");
-    const specificClubRowNumber = specificClubIDs.length + 1;
-    console.log(specificClubIDs.length, "clublength");
+      google.sheets({ version: "v4", auth }).spreadsheets.values.update({
+        spreadsheetId: clubSheet,
+        range: `J${specificClubRowNumber}:J${specificClubRowNumber}`,
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: [[specificClubRowNumber]],
+        },
+      });
+    }
+    // end of the if
 
-    google.sheets({ version: "v4", auth }).spreadsheets.values.update({
-      spreadsheetId: clubSheet,
-      range: `J${specificClubRowNumber}:J${specificClubRowNumber}`,
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: [[specificClubRowNumber]],
-      },
-    });
+    const specificCLubUID = await getOneData(
+      sheets,
+      clubSheet,
+      "Sheet1",
+      UserID,
+      0
+    );
+    const specificClubRowNumber = specificCLubUID[9].toString();
+    console.log(specificClubRowNumber, "specificClubRowNumber 2");
 
     // get what clubs user is in
     const userWhatClubs = await google
