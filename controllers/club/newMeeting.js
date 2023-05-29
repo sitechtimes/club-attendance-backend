f7ab61440987ca47eb1bb840a374e5677c40a5d8
HaoChenSupreme <90791814+HaoChenSupreme@users.noreply.github.com>
2023-05-29 17:51:06 -0400

    changed new meeting

--- controllers/club/newMeeting.js
+++ controllers/club/newMeeting.js
@@ -5,8 +5,9 @@ const { OAuth2Client, AuthClient } = require("google-auth-library");
 const { osconfig } = require("googleapis/build/src/apis/osconfig");
 const client = new OAuth2Client();
 require("dotenv").config({ path: "variables.env" });
-const MainClubData = "1Xm649d7suBlRVjXJeH31k4mAq3NLFV8pW_8QrJ55QpU";
+const MainClubData = "1nxcHKJ2kuOy-aWS_nnBoyk4MEtAk6i1b-_pC_l_mx3g";
 const userDataSheetID = "1noJsX0K3kuI4D7b2y6CnNkUyv4c5ZH-IDnfn2hFu_ws";
+const { getOneData, sheetData } = require("../../utility.js");
 
 const auth = new google.auth.GoogleAuth({
   keyFile: "keys.json",
@@ -26,46 +27,18 @@ exports.newMeeting = async (req, res, next) => {
     console.log(req.body.clubName);
     const clubName = req.body.clubName;
     const newMeeting = req.body.newMeeting;
-    const MainClubData = "1nxcHKJ2kuOy-aWS_nnBoyk4MEtAk6i1b-_pC_l_mx3g";
-    const userDataSheetID = "1noJsX0K3kuI4D7b2y6CnNkUyv4c5ZH-IDnfn2hFu_ws";
+    const sheets = req.object.sheets;
 
-    // This gets the list of every club's name
-    const mainClubDataSheet = await google
-      .sheets({ version: "v4", auth })
-      .spreadsheets.values.get({
-        spreadsheetId: MainClubData,
-        range: "clubData!A2:A",
-      });
-    // clubNameList is the list of club names
-    const clubNameList = mainClubDataSheet.data.values.flat();
-    const clubSorted = clubNameList.sort();
-    console.log(clubSorted);
-
-    // This gets the row number of the club with a binary search, x is the club name
-    function binarySearch(clubSorted, x) {
-      let l = 0,
-        r = clubSorted.length - 1;
-      while (l <= r) {
-        let m = l + Math.floor((r - l) / 2);
-
-        let res = x.localeCompare(clubSorted[m]);
-
-        // Check if x is present at mid
-        if (res == 0) return m;
-
-        // If x greater, ignore left half
-        if (res > 0) l = m + 1;
-        // If x is smaller, ignore right half
-        else r = m - 1;
-      }
-      return -1;
-    }
-    let x = clubName;
-    // result would be the number in the array
-    let result = binarySearch(clubSorted, x);
-    // add 1 to get row number (google sheets don't start with 0)
-    let clubDataRowNumber = result + 2;
-    console.log(clubDataRowNumber);
+    // This gets the clubDataRowNumber (what row the user's club is at on the main sheet)
+    const clubDatas = await getOneData(
+      sheets,
+      MainClubData,
+      "clubData",
+      ClubCode,
+      15
+    );
+    const clubDataRowNumber = clubDatas[16];
+    console.log(clubDataRowNumber, "clubDataRowNumber");
 
     const clubmeeting = await google
       .sheets({ version: "v4", auth })
