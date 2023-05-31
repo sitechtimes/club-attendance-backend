6fcc4f876ea8a87fa707bb29b8280b3ba4795a5f
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2023-05-31 16:58:07 -0400

    Update adminController.js

--- Controllers/user/adminController.js
+++ Controllers/user/adminController.js
@@ -2,16 +2,10 @@
 
 exports.adminCheck = async (req, res, next) => {
   try {
-    if (req.body.user.clientAuthority === undefined) {
-      if (req.body.clientAuthority !== "admin") {
-        return res.json("You are not authorize");
-      }
+    if (req.body.user.clientAuthority !== "admin") {
+      return res.json("You are not authorize");
     }
-    if (req.body.clientAuthority === undefined) {
-      if (req.body.user.clientAuthority !== "admin") {
-        return res.json("You are not authorize");
-      }
-    }
+
     return next();
   } catch (error) {
     console.log(error);
