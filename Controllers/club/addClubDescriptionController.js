d3b75fe1069a184579f2f56d2e936c0e4c280492
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2023-06-04 12:47:05 -0400

    fix user not found

--- Controllers/club/addClubDescriptionController.js
+++ Controllers/club/addClubDescriptionController.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 const CLUB_DATA_SPREADSHEET_ID = `${process.env.NEW_CLUB_DATA_SPREADSHEETID}`;
 //google spreadsheet id for "User Data"
 const USER_DATA_SPREADSHEET_ID = `${process.env.USER_DATA_SPREADSHEET_ID}`;
-const { getOneData, updateValue } = require("../../utility.js");
+const {
+  getOneData,
+  updateValue,
+  ifValueExistBinary,
+} = require("../../utility.js");
 
 exports.addClubDescription = async (req, res, next) => {
   try {
@@ -30,13 +34,14 @@ exports.addClubDescription = async (req, res, next) => {
 exports.test = async (req, res, next) => {
   try {
     const sheets = req.object.sheets;
-    await getOneData(
+    console.log(req.body);
+    const value = await ifValueExistBinary(
       sheets,
       USER_DATA_SPREADSHEET_ID,
-      "userData",
-      req.body.uid,
-      0
+      "userData!A:A",
+      req.body.uid
     );
+    console.log(value);
   } catch (error) {
     console.log(error);
   }
