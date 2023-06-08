bb7d754afdc520e61d86cb08f9c616f97a0df8b0
HaoChenSupreme <90791814+HaoChenSupreme@users.noreply.github.com>
2023-06-08 17:53:27 -0400

    deleted blub fixed

--- controllers/club/deleteClub.js
+++ controllers/club/deleteClub.js
@@ -8,11 +8,7 @@ const client = new OAuth2Client();
 require("dotenv").config({ path: "variables.env" });
 const MainClubData = "1nxcHKJ2kuOy-aWS_nnBoyk4MEtAk6i1b-_pC_l_mx3g";
 const userDataSheetID = "1noJsX0K3kuI4D7b2y6CnNkUyv4c5ZH-IDnfn2hFu_ws";
-const {
-  getOneData,
-  getOneDataSpecial,
-  sheetData,
-} = require("../../utility.js");
+const { getOneData, sheetData } = require("../../utility.js");
 
 const auth = new google.auth.GoogleAuth({
   keyFile: "keys.json",
@@ -26,7 +22,6 @@ exports.removeClub = async (req, res) => {
     console.log(req.body.user.uid);
     const clubName = req.body.clubName;
     const UserID = req.body.user.uid;
-    // const UserID = req.body.user.uid;
     const sheets = req.object.sheets;
 
     // This uses the row number to get the club's sheetid
@@ -37,116 +32,114 @@ exports.removeClub = async (req, res) => {
         range: `A:A`,
       });
     let allClub = allClubs.data.values;
-    console.log(allClub);
+    let allClubString = JSON.stringify(allClub);
+    console.log(allClubString);
+    console.log(!allClubString.includes(clubName), "allClub no includes");
     
     let response;
-    if (!allClub.includes(clubName)) {
+    if (!allClubString.includes(clubName)) {
       response = "Invalid Club Name"
     } else {
-      // This gets the clubDataRowNumber (what row the user's club is at on the main sheet)
-      const clubDatas = await getOneData(
+      // This gets the user's rownumber on the user data sheet
+      const userDatas = await getOneData(
         sheets,
-        MainClubData,
-        "clubData",
-        clubName,
+        userDataSheetID,
+        "userData",
+        UserID,
         0
       );
-      let clubDataRowNumber = clubDatas[17];
-      console.log(clubDataRowNumber, "clubDataRowNumber");
-      // This gets the user's rownumber on the user data sheet
-    const userDatas = await getOneData(
-      sheets,
-      userDataSheetID,
-      "userData",
-      UserID,
-      0
-    );
-    const userRowNumber = userDatas[11];
-    console.log(userRowNumber, "userRowNumber");
+      const userRowNumber = userDatas[11];
+      console.log(userRowNumber, "userRowNumber");
 
-    // This uses the row number to get the club's sheetid
-    const userInfo = await google
-      .sheets({ version: "v4", auth })
-      .spreadsheets.values.get({
-        spreadsheetId: userDataSheetID,
-        range: `userData!J${userRowNumber}:J${userRowNumber}`,
-      });
-    let userClubs = userInfo.data.values[0][0];
-    console.log(userClubs, "userClubs");
-    let arrayUserClubs = JSON.parse(userClubs);
-    console.log(arrayUserClubs);
+      // This uses the row number to get the club's sheetid
+      const userInfo = await google
+        .sheets({ version: "v4", auth })
+        .spreadsheets.values.get({
+          spreadsheetId: userDataSheetID,
+          range: `userData!J${userRowNumber}:J${userRowNumber}`,
+        });
+      let userClubs = userInfo.data.values[0][0];
+      let userClubString = JSON.stringify(userClubs);
+      console.log(userClubs, "userClubs");
 
-    const newArray = arrayUserClubs.filter(
-      (club) => club.clubName !== clubName
-    );
-    console.log(newArray, "newArray");
-    const newArrayString = JSON.stringify(newArray);
+      if (!userClubString.includes(clubName)) {
+        response = "You don't have such club"
+      } else {
+        let arrayUserClubs = JSON.parse(userClubs);
+        console.log(arrayUserClubs);
 
-    let newUserInfo;
-    if (newArrayString === "[]") {
-      newUserInfo = "null";
-    } else {
-      newUserInfo = JSON.stringify(newArray);
-    }
-    console.log(newUserInfo, "newUserInfo");
+        const newArray = arrayUserClubs.filter(
+          (club) => club.clubName !== clubName
+        );
+        console.log(newArray, "newArray");
+        const newArrayString = JSON.stringify(newArray);
 
-    google.sheets({ version: "v4", auth }).spreadsheets.values.update({
-      spreadsheetId: userDataSheetID,
-      range: `userData!J${userRowNumber}:J${userRowNumber}`,
-      valueInputOption: "RAW",
-      resource: {
-        values: [[newUserInfo]],
-      },
-    });
+        let newUserInfo;
+        if (newArrayString === "[]") {
+          newUserInfo = "null";
+        } else {
+          newUserInfo = JSON.stringify(newArray);
+        }
+        console.log(newUserInfo, "newUserInfo");
 
-    // This uses the row number to get the club's sheetid
-    const clubSheetData = await google
-      .sheets({ version: "v4", auth })
-      .spreadsheets.values.get({
-        spreadsheetId: MainClubData,
-        range: `clubData!N${clubDataRowNumber}:N${clubDataRowNumber}`,
-      });
-    let clubSheet = clubSheetData.data.values[0][0];
-    console.log(clubSheet);
+        google.sheets({ version: "v4", auth }).spreadsheets.values.update({
+          spreadsheetId: userDataSheetID,
+          range: `userData!J${userRowNumber}:J${userRowNumber}`,
+          valueInputOption: "RAW",
+          resource: {
+            values: [[newUserInfo]],
+          },
+        });
 
-    let UserIdString = UserID.toString();
-    console.log(UserIdString, "UserIdString");
-    const valueComparing = "1ThZUhPXNg4o-ZqV_JyRZ4HUlK641KiGVDVRx2QcEOvU";
+        // This gets the clubDataRowNumber (what row the user's club is at on the main sheet)
+        const clubDatas = await getOneData(
+          sheets,
+          MainClubData,
+          "clubData",
+          clubName,
+          0
+        );
+        let clubDataRowNumber = clubDatas[17];
+        console.log(clubDataRowNumber, "clubDataRowNumber");
 
-    const specificClubUID = await getOneData(
-      sheets,
-      clubSheet,
-      "Sheet1",
-      UserID,
-      0
-    );
-    console.log(specificClubUID, "specificClubUID");
-    const specificClubRowNumber = specificClubUID[9].toString();
-    console.log(specificClubRowNumber, "specificClubRowNumber 2");
-    const nothing = "null";
+        // This uses the row number to get the club's sheetid
+        const clubSheetData = await google
+          .sheets({ version: "v4", auth })
+          .spreadsheets.values.get({
+            spreadsheetId: MainClubData,
+            range: `clubData!N${clubDataRowNumber}:N${clubDataRowNumber}`,
+          });
+        let clubSheet = clubSheetData.data.values[0][0];
+        console.log(clubSheet);
 
-    google.sheets({ version: "v4", auth }).spreadsheets.values.update({
-      spreadsheetId: clubSheet,
-      range: `Sheet1!A${specificClubRowNumber}:J${specificClubRowNumber}`,
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: [
-          [
-            nothing,
-            nothing,
-            nothing,
-            nothing,
-            nothing,
-            nothing,
-            nothing,
-            nothing,
-            nothing,
-            nothing,
-          ],
-        ],
-      },
-    });
-    res.json("Finish deleting");
+        let UserIdString = UserID.toString();
+        console.log(UserIdString, "UserIdString");
+        const valueComparing = "1ThZUhPXNg4o-ZqV_JyRZ4HUlK641KiGVDVRx2QcEOvU";
+
+        const specificClubUID = await getOneData(
+          sheets,
+          clubSheet,
+          "Sheet1",
+          UserID,
+          0
+        );
+        console.log(specificClubUID, "specificClubUID");
+        const specificClubRowNumber = specificClubUID[9].toString();
+        console.log(specificClubRowNumber, "specificClubRowNumber 2");
+        const nothing = "null";
+
+        google.sheets({ version: "v4", auth }).spreadsheets.values.update({
+          spreadsheetId: clubSheet,
+          range: `Sheet1!A${specificClubRowNumber}:J${specificClubRowNumber}`,
+          valueInputOption: "USER_ENTERED",
+          resource: {
+            values: [[ nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing ]],
+          },
+        });
+        response = "Deleted"
+      };
+    };
+    res.json(response);
   } catch (error) {
     console.log(error);
   }
